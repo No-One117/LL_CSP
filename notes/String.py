87247a1e41875c694ac5288ee9b054fb2a195115
No-One117 <lucas.law@ucas-edu.net>
2025-09-08 13:44:46 -0600

    wiwowowwoow

--- notes/String.py
+++ notes/String.py
@@ -1,2 +1,26 @@
-#LL 6th String.py
+#LL 6th String Notes.py
 
+# what makes some a string
+# a string is a collection of charectors 
+#ex "hi loser dum dum" or 'hi loser dum dum'
+
+#examples of strings
+name = input ("whats your name")
+print(f"Nice to meet you {name}")
+sentence = ("brown fox jumps over the lazy dog")
+
+
+# why do we have strings?
+
+#how do stupid proofing and sanitization relate to eachother
+#users = stupid
+
+
+#what is debugging
+
+#how do you debug the differant typ of errors
+ #find()
+ #concanate (ADD)
+ # capitilize only first letter
+
+# upper makes all of the letters in an input statement uppercase and lower does it to lowercase
