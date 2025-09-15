a4febd876f6cf2d75dc16ce56938195c47a81be9
No-One117 <lucas.law@ucas-edu.net>
2025-09-15 16:03:48 +0000

    hi

--- practices/silly_sentences.py
+++ practices/silly_sentences.py
@@ -15,7 +15,7 @@ soft = input("enter the name of something soft")
 # Create and display the silly story
 print("\nHere’s your silly story:\n")
 print(f"when i woke up i was inside a {place} and the smell of {food} filled the air the {place} was covered in white {soft}. ")
-print(f"I was wearing a leather {noun} and saw {relative} behind a window so i {verb} over and try to hug but the jacket didnt let me ")
+print(f"I was wearing a leather {noun} and saw {relative} behind a window so i {verb} over and tryed to hug them but the jacket didnt let me ")
 print(f"then a wierd man holding an {animal} came in and gave me a shot and i felt sleepy and then i woke up and {verb} away")
 
 
