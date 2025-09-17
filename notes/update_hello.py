7575a910d733649fe67fda47ff1a1da546f5bce8
No-One117 <lucas.law@ucas-edu.net>
2025-09-17 13:44:17 -0600

    hi

--- notes/update_hello.py
+++
@@ -1,6 +0,0 @@
-# LL 6th Hello world update
-
-def clean(NAME):
-    print(f"hello {NAME}")
-
-def greet(NAME):
---
+++ practices/conditional.py
@@ -0,0 +1,60 @@
+# LL 6th Conditional
+
+#this is a conditonal! (look for if and else statements)
+x = 5
+if x > 5:
+    print("x is greater than 5")
+elif x == 5:
+    print("x is equal to 5")
+else:
+    print("x is not greater than 5")
+
+
+
+name = "john"
+
+if name == ("johnathon"):
+    print("you are the chief")
+elif name == ("john"):
+    print("hello cheif")
+else:
+    print(f"hello {name}, i dont know what you are")
+
+# What puts something inside of the “if” statement?
+
+# to put something into an if statement you need a conditional expression that has a true or false value.
+
+# What do if statements do?
+
+# if statements check if something is true and prints something because of it
+
+# What are boolean statements? 
+
+# expressions that evaluate to one of two possible values like True or False
+
+# What do else statements do?
+
+# else statements check if the thing you typed in is correct and if it isnt it prints something like please type only letters
+
+# What kind of statement do you use if you have more than 2 needed outcomes?
+
+# elif
+
+# What do each of the different symbols mean in conditionals?
+# < less than                               |
+# > greater than         <- comparison ops  |   
+# <= less than or equal too                 |  
+# >= greater than or equal to               | 
+# == equal too                              | 
+# != not equal too                          | 
+# What are the 3 logical operators?
+ 
+# and,or,not
+
+# What are logical operators for?
+
+# to combine and manipulate Boolean expressions
+
+# What does a nested conditional statement do?
+
+# allows a programto evaluate multiple condidtions sequentially
