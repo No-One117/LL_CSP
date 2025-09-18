4b12d8965285eaf3cc6d3f5e864871e4a99b698a
No-One117 <lucas.law@ucas-edu.net>
2025-09-18 13:35:36 -0600

    hellobutnot

--- practices/old_enough.py
+++ practices/old_enough.py
@@ -2,13 +2,16 @@
 
 age = int(input("Enter your age: "))
 
+if age >=21:
+    print("your old enough to drink the fun juice")
+
 if age >= 18:
-    print("You are old enough to vote.")
+    print("You are old enough to vote, but not drink the fun juice")
 elif age >= 16:
-    print("You are old enough to drive.")
+    print("You are old enough to drive, but not old enough to vote.")
 elif age >= 15:
-    print("You are old enough to get a learner's permit.")
+    print("You are old enough to get a learner's permit, but not a drivers license nor vote.")
 elif age >= 5:
-    print("You are old enough to go to school.")
+    print("You are old enough to go to school, but not old enough to have a learners permit, vote or drive")
 else:
     print("You are not old enough for school, a permit, driving, or voting.")
