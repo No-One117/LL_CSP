6224385f3eb866dcdd77270a5abe3851be5eeda7
No-One117 <lucas.law@ucas-edu.net>
2025-09-18 18:41:16 -0600

    Update update_helloworldcode.py

--- practices/update_helloworldcode.py
+++ practices/update_helloworldcode.py
@@ -1,10 +1,18 @@
-def say_hello(name):
-  """Prints a greeting message to the given name."""
-  print(f"Hello, {name}!")
+# Define a function that prints a hello message
+def sayHello(name):
+    print("Hello, " + name + "!")
 
-# Call the function 5 times with different names from user input
-for i in range(5):
-  user_name = input(f"Enter name {i+1}: ")
-  say_hello(user_name)
+# Repeat the following steps 5 times
+count = 1
+while count <= 5:
+    # Ask the user to enter a name
+    name = input("Enter name " + str(count) + ": ")
+
+    # Call the function to say hello
+    sayHello(name)
+
+    # Increase the count by 1
+    count = count + 1
+
 
 
