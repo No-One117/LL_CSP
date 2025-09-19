afcc25f5a8d314c8a311e8d53861bfe651c5e7b9
No-One117 <lucas.law@ucas-edu.net>
2025-09-18 18:47:49 -0600

    Update update_helloworldcode.py

--- practices/update_helloworldcode.py
+++ practices/update_helloworldcode.py
@@ -1,18 +1,11 @@
 # Define a function that prints a hello message
 def sayHello(name):
-    print("Hello, " + name + "!")
+    print("Hello, " + name ")
 
-# Repeat the following steps 5 times
-count = 1
-while count <= 5:
+# Repeat the steps 5 times using a for loop
+for count in range(1, 6):
     # Ask the user to enter a name
     name = input("Enter name " + str(count) + ": ")
 
     # Call the function to say hello
     sayHello(name)
-
-    # Increase the count by 1
-    count = count + 1
-
-
-
