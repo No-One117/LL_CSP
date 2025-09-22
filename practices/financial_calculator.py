072e952ae31f755521dcd64b898733a72a62d687
No-One117 <lucas.law@ucas-edu.net>
2025-09-22 23:08:36 +0000

    hi

--- practices/financial_calculator.py
+++ practices/financial_calculator.py
@@ -1,10 +1,15 @@
 # LL 6th Period Finance Calculator
+def calculate_budget(income, rent, utilities, groceries):
+    return income - rent - utilities - groceries
+
 
 income = float(input("Enter your monthly income: "))
 rent = float(input("Enter your monthly rent: "))
 utilities = float(input("Enter your monthly utilities: "))
 groceries = float(input("Enter your monthly groceries: "))
-remaining_funds = income - rent - utilities - groceries
+remaining_funds = calculate_budget(income, rent, utilities, groceries)
+
+
 
 print(f"Your Monthly Budget is {remaining_funds} ")
 print("Total income: $", income)
