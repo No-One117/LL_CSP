cc6ac3b9917e1801e23566f8ea551840db879c3d
No-One117 <lucas.law@ucas-edu.net>
2025-10-02 13:44:07 -0600

    variables

---
+++ c_programming/notes/variables.c
@@ -0,0 +1,43 @@
+// LL 6th Variables notes
+#include <stdio.h>
+
+int main(void){
+    int num = 4;
+    float pi = 3.14;
+    char grade = 'A';//will only hold one character
+    char name[] = " Lucas";//string is an array of characters
+    //bool passing = true;
+    printf("%d", num);
+
+    return 0;
+}
+
+//What is the main difference between declaring variables in Python and C?
+//in c you have to specify the data type of the variable
+//In C, what is the purpose of specifying a data type when declaring a variable?
+//Specifying a data type in C determines the kind of data the variable can hold and how much memory it will use.
+//List three common data types used in C and their corresponding format specifiers for printf().
+//int (%d), float (%f), char (%c)
+//How do you declare and initialize an integer variable named "age" with the value 25 in C?
+//int age = 25;
+//What is the difference between printf() and scanf() functions in C?
+// printf() is used to output data to the console, while scanf() is used to read input from the user.
+//Write a C program that asks the user for their name and age, then prints a greeting message with their information.
+//
+//How do you add comments in C? What are the two types of comments? 
+//You can add comments in C using // for single-line comments and /* */ for multi-line comments.
+//What is the purpose of the #include <stdio.h> line at the beginning of a C program?
+//
+//In C, what is the significance of the main() function?
+
+//Write a C program that calculates the area of a rectangle. Ask the user to input the length and width, then display the result.
+
+//What is the difference between %d and %f format specifiers in printf()?
+
+//How do you print a newline character in C?
+
+//Write a C program that converts temperature from Celsius to Fahrenheit. Ask the user for input in Celsius and display the result in Fahrenheit.
+
+//What is the purpose of the & symbol when using scanf() to get user input?
+
+//How do you declare a constant in C? Provide an example.
