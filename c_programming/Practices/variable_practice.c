217ee6427e8229ad2bc061e141fcb32f7a42e759
No-One117 <lucas.law@ucas-edu.net>
2025-10-03 13:33:35 -0600

    variable things (for c)

---
+++ c_programming/Practices/variable_practice.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+
+    char name[] = "Lucas";
+    char breakfast[] = "egg sandwich";
+    char fav_color[] = "purple";
+    char school[] = "UCAS";
+    char color[] = "Blue";
+    char subject[] = "biology";
+    
+    int number = 14;
+    int year = 2025;
+    int age = 14;
+    
+    float gpa = 3.7;
+
+    printf("Name: %s\n", name);
+    printf("Number (1-20): %d\n", number);
+    printf("GPA: %f\n", gpa);
+    printf("Breakfast: %s\n", breakfast);
+    printf("Favorite Color: %s\n", fav_color);
+    printf("School Name: %s\n", school);
+    printf("Year: %d\n", year);
+    printf("Eye Color: %s\n", color);
+    printf("Age: %d\n", age);
+    printf("Favorite Subject: %s\n", subject);
+
+    return 0;
+}
--- c_programming/notes/variables.c
+++ c_programming/notes/variables.c
@@ -13,31 +13,24 @@ int main(void){
 }
 
 //What is the main difference between declaring variables in Python and C?
-//in c you have to specify the data type of the variable
+// the main difference is that in C you have to specify the data type of the variable when you declare it, while in Python you do not have to specify the data type
 //In C, what is the purpose of specifying a data type when declaring a variable?
-//Specifying a data type in C determines the kind of data the variable can hold and how much memory it will use.
-//List three common data types used in C and their corresponding format specifiers for printf().
-//int (%d), float (%f), char (%c)
+// the purpose of specifying a data type when declaring a variable in C is to tell the compiler what kind of data the variable will hold.
 //How do you declare and initialize an integer variable named "age" with the value 25 in C?
-//int age = 25;
+// you would declare it like this: int age = 25;
 //What is the difference between printf() and scanf() functions in C?
-// printf() is used to output data to the console, while scanf() is used to read input from the user.
-//Write a C program that asks the user for their name and age, then prints a greeting message with their information.
-//
-//How do you add comments in C? What are the two types of comments? 
-//You can add comments in C using // for single-line comments and /* */ for multi-line comments.
+// the printf() function is used to print output to the console, while the scanf() function is used to get input from the user.
+//How do you add comments in C?
+// this is a comment and a multi-line comment is like this ( /* this is a multi-line comment */ )
 //What is the purpose of the #include <stdio.h> line at the beginning of a C program?
-//
+// the purpose of the #include <stdio.h> line at the beginning of a C program is to include the standard input/output library, which contains functions like printf() and scanf().
 //In C, what is the significance of the main() function?
-
-//Write a C program that calculates the area of a rectangle. Ask the user to input the length and width, then display the result.
-
+// the significance of the main() function in C is that it is the entry point of the program, where the execution of the program begins.
 //What is the difference between %d and %f format specifiers in printf()?
-
+// the %d format specifier is used to print integers, while the %f format specifier is used to print floating-point numbers.
 //How do you print a newline character in C?
-
-//Write a C program that converts temperature from Celsius to Fahrenheit. Ask the user for input in Celsius and display the result in Fahrenheit.
-
+// to print a newline character in C, you use the \n escape thingy
 //What is the purpose of the & symbol when using scanf() to get user input?
-
+// to get user input is to provide the address of the variable where the input value will be stored.
 //How do you declare a constant in C? Provide an example.
+// you declare a constant in C using the const keyword, const int max_age = 100;
