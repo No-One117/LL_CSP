02734ee10e34af07a6dfff902f310bdcdaed6f5e
No-One117 <lucas.law@ucas-edu.net>
2025-10-14 13:45:31 -0600

    commit

---
+++ c_programming/notes/functions.c
@@ -0,0 +1,45 @@
+// LL 6th Notes on Functions
+
+
+
+
+//What a function is
+
+ // A function is a block of code that performs a specific task.
+
+//Why we use functions
+
+ // To make our code more organized and reusable.
+
+//How to write a function in C
+
+ // return_type function_name(parameter1_type parameter1_name, parameter2_type parameter2_name) {
+    // code to be executed
+    // return value;
+//}
+
+ //What are arguments and parameters?
+
+// Parameters are the variables defined in the function definition.
+
+ //How do arguments and parameters work together?
+
+// Arguments are the actual values passed to the function when it is called.
+
+ //How to use parameters and arguments in c
+
+ // int add(int a, int b) {
+    // return a + b;
+ //}
+
+//What are return statements?
+
+ // Return statements are used to return a value from a function.
+
+//How do return statements change how you define a function in C?
+
+ // You need to specify the return type of the function.
+
+//What do return statements do with the information?
+
+ // They send the value back to the place where the function was called.
