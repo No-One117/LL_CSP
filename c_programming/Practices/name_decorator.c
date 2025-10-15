c28edc05836e082945d56c241916739e2b0164ac
No-One117 <lucas.law@ucas-edu.net>
2025-10-15 13:02:52 -0600

    name

--- c_programming/Practices/name_decorator.c
+++ c_programming/Practices/name_decorator.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+
+    char greeting[100] = ""; 
     char name[50];
 
     printf("What is your name: ");
     fgets(name, sizeof(name), stdin);
 
-    printf(">>>%s<<<\n", name);
+    name[strcspn(name, "\n")] = 0;
+
+    strcat(greeting, name);
+
+    printf(":(:(%s(:(:\n", greeting);
 
     return 0;
 }
